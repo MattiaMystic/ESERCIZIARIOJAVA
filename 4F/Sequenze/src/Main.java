85b6b6e26e1fe6005729610e1d65f6650b4c02d9
MattiaMystic <150335971+MattiaMystic@users.noreply.github.com>
2024-09-24 09:55:31 +0200

    Update Main.java

--- 4F/Sequenze/src/Main.java
+++ 4F/Sequenze/src/Main.java
@@ -2,50 +2,61 @@ import static Tools.Utility.*;
 /*
 Generare un valore contenente numeri casuali
 */
+
 import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         Scanner tastiera = new Scanner(System.in);
-        String[] opzioni = {"Menu", "1 Genera Random", "2 Visualizzazione", "3 Fine"};
+        Scanner tastiera2 = new Scanner(System.in);
+        String[] opzioni = {"Menu", "1 Genera Random", "2 Visualizzazione", "3 ricerca numero","4 fine"};
         boolean esci = true;
-        int[] nEstratti = null;
         final int MAXNUMERI = 100;
         int[] nEstratti2 = new int[MAXNUMERI];
-        //int[] nEstratti2;
+        int n;
+        //int[] nEstratti; senza new perchè fatta nel metodo su nGenerati
         do {
             switch (menu(opzioni, tastiera)) {
                 case 1:
                     System.out.println("Generazione");
                 /*for (int i=0; i<MAXNUMERI; i++)
                     nGenerati[i] = numeroRandom.nextInt(0,MAXNUMERI)+1;
-                 */
+                 */ //nEstratti=generaNumeri(MAXNUMERI)
                     generaNumeri2(nEstratti2);
                     break;
                 case 2:
                     System.out.println("Visualizzazione");
-                    for (int i = 0; i < nEstratti2.length; i++) {
+                    /*for (int i = 0; i < nEstratti2.length; i++) {
                         System.out.println(nEstratti2[i]);
-                    }
+                    }*/
+                    Visualizza(nEstratti2);
                     break;
 
                 case 3:
+                    System.out.println("Ricerca numero e posizione in cui si trova");
+                    System.out.println("Quale numero vuoi cercare?");
+                     n=Integer.parseInt(tastiera2.nextLine());
+                    Ricerca(nEstratti2,n);
+                    break;
+                case 4:
                     System.out.println("Fine");
                     esci = false;
                     break;
             }
         } while (esci);
     }
+    //metodo 1
 
     /* public static int[] generaNumeri(int nNumeri) {
          Random numeroRandom = new Random();
-         int[] nGenerati = new int[nNumeri];
+         int[] nGenerati = new int[nNumeri];// la new
          for (int i = 0; i < nNumeri; i++)
              nGenerati[i] = numeroRandom.nextInt(0, nNumeri) + 1;
-         return nGenerati;
+         return nGenerati; //usi return se fai la new
      }*/
- public static void generaNumeri2(int[] vettore) {
+    //int [] vettore=new int [MAXNUMERI]
+    public static void generaNumeri2(int[] vettore) {
         Random numeroRandom = new Random();
         int numero;
         int cont=0;
@@ -67,6 +78,32 @@ public class Main {
             }
         } while (cont < vettore.length);
     }
+    public static void Visualizza(int[] vettore){
+        int cont=0;
+        for(int i=0; i<vettore.length;i++){
+            System.out.printf(vettore[i]+" ");
+            cont++;
+            if(cont==10){
+                cont=0;
+                System.out.println();
+            }
+
+        }
+    }
+    public static void Ricerca(int[] vettore,int ricerca){
+        boolean trovato=false;
+        for(int i=0; i<vettore.length;i++){
+            if(ricerca==vettore[i]){
+
+                System.out.println("Il numero è presente ed è in posizione "+i);
+               trovato=true;
+            }
+            }
+        if(!trovato){
+            System.out.println("Numero non presente");
+        }
+
+        }
+    }
 
 
-}
