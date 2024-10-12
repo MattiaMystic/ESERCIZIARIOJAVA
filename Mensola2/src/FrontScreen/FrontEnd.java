7c5c162c5a3c4c2a508e3973e18a550a37a9c17d
MattiaMystic <150335971+MattiaMystic@users.noreply.github.com>
2024-10-12 09:22:49 +0200

    Add files via upload

--- Mensola2/src/FrontScreen/FrontEnd.java
+++ Mensola2/src/FrontScreen/FrontEnd.java
@@ -1,8 +1,14 @@
 package FrontScreen;
-import java.security.Key;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import Mensola.*;
 import Tools.Utility.*;
+
+import static Tools.Utility.menu;
+
+
 public class FrontEnd {
     public static Libro LeggiLibro(Scanner Keyboard){
         Libro l = new Libro();
@@ -13,9 +19,12 @@ public class FrontEnd {
         System.out.println("Inserisci il tipo:");
        String[] opzioni={"Tipo libro","1 Romanzo","2 Manuale","3 Triller"};
         switch (menu(opzioni,Keyboard)){
-            case 1 -> l.tipo =Tipologia.Romanzo
-
+            case 1 -> l.tipo =Tipologia.Romanzo;
+            case 2 -> l.tipo =Tipologia.Manuale;
+            case 3 -> l.tipo =Tipologia.Thriller;
         }
+        System.out.println("Inserisci la data di pubblicazione in formato dd-MM-yyyy");
+        l.dataDiPubblicazione= LocalDate.parse(Keyboard.nextLine(), DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         return l;
     }
 }
--- Mensola2/src/Mensola/Libro.java
+++ Mensola2/src/Mensola/Libro.java
@@ -1,20 +1,21 @@
 package Mensola;
 
-enum Tipologia{
-    Romanzo,
-    Manuale,
-    Triller;
-}
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Libro {
 
     public String Autore;
     public String Titolo;
     final double PREZZOPAGINA=0.05;
     public int pagine;
+    public LocalDate dataDiPubblicazione;
+    public Tipologia tipo;
+    private DateTimeFormatter dataFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
 
     public String FormattaDati(){
-        return String.format("Autore : %s , Titolo: %s, PREZZOPAGINA %.2f, Pagine:%d ",Autore,Titolo,PREZZOPAGINA*(double)pagine,pagine);
+        return String.format("Autore : %s , Titolo: %s, PREZZOPAGINA %.2f, Pagine:%d , tipo: %s, Data: %s ",Autore,Titolo,PREZZOPAGINA*pagine,pagine,tipo,dataDiPubblicazione.format(dataFormat));
     }
 
 
