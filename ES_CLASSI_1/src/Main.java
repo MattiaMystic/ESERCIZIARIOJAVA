f0325821d7ccc2563e9370d1990f9e4b374d5d0e
MattiaMystic <150335971+MattiaMystic@users.noreply.github.com>
2024-11-12 12:14:58 +0100

    Update Main.java

--- ES_CLASSI_1/src/Main.java
+++ ES_CLASSI_1/src/Main.java
@@ -14,12 +14,14 @@ public class Main {
             System.out.println(e.getMessage());
 
         }*/
-try {
-    System.out.println("Inserisci le coordinate del punto X");
-    double coordX = sc.nextDouble();
-    System.out.println("Inserisci le coordinate del punto Y");
-    double coordY = sc.nextDouble();
-    Punto p = new Punto(coordX, coordY);
+
+            /*
+
+            System.out.println("Inserisci le coordinate del punto X");
+            double coordX = sc.nextDouble();
+            System.out.println("Inserisci le coordinate del punto Y");
+            double coordY = sc.nextDouble();
+            Punto p = new Punto(coordX, coordY);
        /* if(p.getY()>0 && p.getX()>0){
             System.out.println("Si trova nel 1 quadrante");
         }
@@ -45,11 +47,15 @@ try {
         } else{
             System.out.println("Il punto si trova nell'origine degli assi");
         }
-*/
-    System.out.println(p.trovaQuadrante());
-    System.out.println(p.toString());
-}catch(Exception e){
-    System.out.println(e.getMessage());
-}
+
+            System.out.println(p.trovaQuadrante());
+            System.out.println(p.toString());*/
+        try{
+            Punto punto = new Punto(20,10);
+            System.out.println(punto.toString());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
