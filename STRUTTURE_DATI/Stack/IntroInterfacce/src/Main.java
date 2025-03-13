5f55f88e51b1a15339097b955be6ddb8cba1f170
MattiaMystic <150335971+MattiaMystic@users.noreply.github.com>
2025-03-13 13:19:33 +0100

    Add files via upload

---
+++ STRUTTURE_DATI/Stack/IntroInterfacce/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Cubo c= new Cubo(3,4);
+        System.out.println(c.peso());
+        Sfera s = new Sfera(3,4);
+        System.out.println(s.peso());
+
+
+    }
+}
---
+++ STRUTTURE_DATI/Stack/IntroInterfacce/src/Sfera.java
@@ -0,0 +1,24 @@
+public class Sfera extends Solido {
+    private double raggio;
+
+    protected Sfera(double pesoSpecifico, double raggio) {
+        super(pesoSpecifico);
+        this.raggio = raggio;
+    }
+
+    @Override
+    public double volume() {
+
+        return (double)4/3*Math.PI*Math.pow(raggio,3);
+    }
+
+    @Override
+    public double superficie() {
+        return (double)4*Math.PI*Math.pow(raggio,2);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ "Sfera: La mia superficie è " + superficie()+" e il mio volume è " + volume();
+    }
+}
