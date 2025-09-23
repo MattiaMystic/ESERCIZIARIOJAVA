a73dd91c034f05736e4b36ba5a5eee5edd229091
Mattia <mattia.pavarin@gmail.com>
2025-09-23 16:24:07 +0200

    sistemazione 23/09/2025

--- 5F_2025-2026/Es_2_info/script.js
+++ 5F_2025-2026/Es_2_info/script.js
@@ -4,29 +4,22 @@ const prova2Button = document.getElementById("Prova2");
 const prova3Button = document.getElementById("Prova3");
 const prova4Button = document.getElementById("Prova4");
 const output = document.getElementById("output");
+const colore= document.getElementById("colore");
 
 provaButton.addEventListener("click", function() {
     x++;  
-
-        let text=x;
-        output.textContent = text;  
-  
+    output.textContent = x;  
 });
+
 prova2Button.addEventListener("click", function() {
-  x--;  
-  
-        let text=x;
-        output.textContent = text;  
-   
+    x--;  
+    output.textContent = x;  
 });
+
 prova3Button.addEventListener("click", function() {
-   background-color: red;
-   
-
-  
+    colore.style.backgroundColor = "red";
 });
+
 prova4Button.addEventListener("click", function() {
-  background-color: blue;
-   
+    colore.style.backgroundColor = "blue";
 });
-
